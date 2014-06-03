0660aa247b5990cc597a71e06c2338628b73d8a2
Thanat Sirithawornsant <rawsec@gmail.com>
2014-06-03 14:26:37 +0700

    Initial

--- testDjango/settings.py
+++ testDjango/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testApp',
+    'pages',
 )
 
 MIDDLEWARE_CLASSES = (
--- testDjango/urls.py
+++ testDjango/urls.py
@@ -3,11 +3,27 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+# /home
+# /account
+# 	/orderhistory
+# /browse
+# 	/search/$keyword
+# /product/$pid
+# /order/$oid
+# /checkout
+# /payment
+# /login
+# /logout
+# /register
+# /contact
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'testDjango.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', include('pages.urls')),
+    url(r'^home/', include('pages.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
